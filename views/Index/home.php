56468d57bab0b7630ea2bba4110d43bca0fecc13
Cameron Condry <ccondry@pltw.org>
2014-02-08 15:32:07 -0500

    Adding base css styles.

--- views/Index/home.php
+++ views/Index/home.php
@@ -2,5 +2,38 @@
 <?php $this->title = 'Home' ?>
 
 <?php $this->start('content') ?>
-    <p>Home Template Loaded</p>
+    <aside class="sidebar-left">
+        <h1>Cameron Condry</h1>
+        <h4>Web Developer</h4>
+        <ul>
+            <li>Some</li>
+            <li>Data</li>
+        </ul>
+    </aside>
+
+    <div class="main-content">
+        <section class="main-content-inner">
+            <h1>Cameron Condry</h1>
+            <h4>Web Developer</h4>
+            <p>Describe Current Job: Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi atque dignissimos distinctio fugiat impedit incidunt iste iure laudantium maiores nesciunt odio praesentium quisquam quo quod, repellat, tenetur velit voluptatum. Aperiam cum dignissimos dolorum earum eos fugiat ipsa, itaque odit perferendis possimus praesentium saepe veritatis vitae! Cumque ea maxime non quae, quia quis temporibus tenetur voluptate. Consectetur cupiditate dignissimos excepturi inventore molestias nisi nobis officia provident, quas qui quibusdam quis repellat repellendus rerum totam veritatis.</p>
+
+            <h2>Projects</h2>
+            <h3>Project 1:</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti ducimus illo non quas reiciendis? Accusamus cupiditate deleniti dolor doloribus exercitationem in laboriosam maxime natus neque nobis non quae, saepe sed similique sint vel veniam? Aut ea earum quia recusandae temporibus. Corporis culpa debitis, distinctio eos error illo porro repellendus tenetur!</p>
+            <h3>Project 2:</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias, aperiam architecto aspernatur at commodi cumque dolorum eos eveniet excepturi expedita explicabo hic ipsa labore laborum magni maiores molestias natus necessitatibus, nesciunt obcaecati omnis pariatur perspiciatis placeat quae quidem quis quos ratione repellendus sed, sequi tempora tempore velit vitae voluptatum?</p>
+        </section>
+        <aside class="sidebar-right">
+            <ul>
+                <li><a href="#">Test Link</a></li>
+                <li><a href="#">Test Link</a></li>
+            </ul>
+        </aside>
+        <div class="clearfix"></div>
+    </div>
+
+
+
+    <div class="clearfix"></div>
+
 <?php $this->end() ?>
--- views/base.php
+++ views/base.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="utf-8" />
     <meta name="description" content="">
-    <title>Cameron Condry - <?= $this->title ?> </title>
+    <title>Cameron Condry - <?= $this->title ?></title>
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans|Roboto' rel='stylesheet' type='text/css'>
+    <link href="/assets/css/normalize.css" type="text/css" rel="stylesheet">
+    <link href="/assets/css/main.css" type="text/css" rel="stylesheet">
 </head>
 <body>
 <div id="content">
